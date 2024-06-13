41be3056d91264e073c6eda6a3fea758d226ccbe
diya42 <144512838+diya42@users.noreply.github.com>
2024-06-13 12:11:55 +0400

    Create python.py
    sort 20 nos

---
+++ python.py
@@ -0,0 +1,5 @@
+num = [14, 7, 12, 8, 6, 19, 20, 15, 3, 11, 2, 17, 5, 10, 9, 4, 18, 13, 16, 1]
+# Sort the list of numbers
+sortednum = sorted(num)
+print("Original list:", num)
+print("Sorted list:", sortednum)
